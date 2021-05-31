ffa2db03c50a1c8058454bca92cfe2c4aab0fee4
Humberto Cordeiro <humberto_cordeiro@outlook.com>
2021-05-31 13:55:26 -0300

    alterado arquivos index.html e main.js

---
+++ JS/main.js
@@ -0,0 +1,12 @@
+var nome = "Humberto Cordeiro"
+var idade = 58
+var idade2 = 15
+var frase = "O japão é o maior time do mundo!"
+//alert(nome + " Esta com " + idade + "anos")
+//alert(idade + idade2)
+console.log(nome);
+console.log(idade + idade2);
+console.log(frase.replace("japão", "Brasil"));
+//alert(frase.replace("japão", "Brasil"))
+console.log(frase.toLowerCase())
+
